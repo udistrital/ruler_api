efb1e77acf0b3a4aca53dfb0ada6040f274dc3fc
faparraf <faparraf@gmail.com>
2017-03-02 12:02:17 -0500

    Commit copiando de apdaza/rulerV3 y modificando variables de entorno y configuracion

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func init() {
-	orm.RegisterDataBase("default", "postgres", "postgres://postgres:sistemasoas@127.0.0.1/ruler?sslmode=disable")
+	orm.RegisterDataBase("default", "postgres", "postgres://"+beego.AppConfig.String("db_user")+":"+beego.AppConfig.String("db_pass")+"@"+beego.AppConfig.String("db_url")+"/"+beego.AppConfig.String("db_name")+"?sslmode=disable&search_path="+beego.AppConfig.String("db_schemas")+")
 }
 
 func main() {
