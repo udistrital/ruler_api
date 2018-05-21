7f4f41b65b82690f981b4cb6519395bdaa882bbe
Fabio Parra <faparraf@gmail.com>
2018-05-21 15:40:08 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ set -u
 set -o pipefail
 
 if [ -n "${PARAMETER_STORE:-}" ]; then
-  export RULER_API__PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/ruler_api/db/username --output text --query Parameter.Value)"
-  export RULER_API__PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/ruler_api/db/password --output text --query Parameter.Value)"
+  export RULER_DB_USER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/ruler_api/db/username --output text --query Parameter.Value)"
+  export RULER_DB_PASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/ruler_api/db/password --output text --query Parameter.Value)"
 fi
 
 exec ./main "$@"
