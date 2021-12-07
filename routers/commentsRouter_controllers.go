180a1eb0f3749b22ee3f7b3151c1451be5d080ab
Alex Bustos <alexfbp@gmail.com>
2021-12-07 12:33:38 -0500

    fix: `bee run -downdoc=true -gendoc=true` faltante

--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -2,113 +2,144 @@ package routers
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context/param"
 )
 
 func init() {
 
 	beego.GlobalControllerRouter["github.com/udistrital/ruler_api/controllers:DominioController"] = append(beego.GlobalControllerRouter["github.com/udistrital/ruler_api/controllers:DominioController"],
 		beego.ControllerComments{
-			Method: "Post",
-			Router: `/`,
+			Method:           "Post",
+			Router:           "/",
 			AllowHTTPMethods: []string{"post"},
-			Params: nil})
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 
 	beego.GlobalControllerRouter["github.com/udistrital/ruler_api/controllers:DominioController"] = append(beego.GlobalControllerRouter["github.com/udistrital/ruler_api/controllers:DominioController"],
 		beego.ControllerComments{
-			Method: "GetOne",
-			Router: `/:id`,
+			Method:           "GetAll",
+			Router:           "/",
 			AllowHTTPMethods: []string{"get"},
-			Params: nil})
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 
 	beego.GlobalControllerRouter["github.com/udistrital/ruler_api/controllers:DominioController"] = append(beego.GlobalControllerRouter["github.com/udistrital/ruler_api/controllers:DominioController"],
 		beego.ControllerComments{
-			Method: "GetAll",
-			Router: `/`,
+			Method:           "GetOne",
+			Router:           "/:id",
 			AllowHTTPMethods: []string{"get"},
-			Params: nil})
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 
 	beego.GlobalControllerRouter["github.com/udistrital/ruler_api/controllers:DominioController"] = append(beego.GlobalControllerRouter["github.com/udistrital/ruler_api/controllers:DominioController"],
 		beego.ControllerComments{
-			Method: "Put",
-			Router: `/:id`,
+			Method:           "Put",
+			Router:           "/:id",
 			AllowHTTPMethods: []string{"put"},
-			Params: nil})
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 
 	beego.GlobalControllerRouter["github.com/udistrital/ruler_api/controllers:DominioController"] = append(beego.GlobalControllerRouter["github.com/udistrital/ruler_api/controllers:DominioController"],
 		beego.ControllerComments{
-			Method: "Delete",
-			Router: `/:id`,
+			Method:           "Delete",
+			Router:           "/:id",
 			AllowHTTPMethods: []string{"delete"},
-			Params: nil})
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 
 	beego.GlobalControllerRouter["github.com/udistrital/ruler_api/controllers:PredicadoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/ruler_api/controllers:PredicadoController"],
 		beego.ControllerComments{
-			Method: "Post",
-			Router: `/`,
+			Method:           "Post",
+			Router:           "/",
 			AllowHTTPMethods: []string{"post"},
-			Params: nil})
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 
 	beego.GlobalControllerRouter["github.com/udistrital/ruler_api/controllers:PredicadoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/ruler_api/controllers:PredicadoController"],
 		beego.ControllerComments{
-			Method: "GetOne",
-			Router: `/:id`,
+			Method:           "GetAll",
+			Router:           "/",
 			AllowHTTPMethods: []string{"get"},
-			Params: nil})
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 
 	beego.GlobalControllerRouter["github.com/udistrital/ruler_api/controllers:PredicadoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/ruler_api/controllers:PredicadoController"],
 		beego.ControllerComments{
-			Method: "GetAll",
-			Router: `/`,
+			Method:           "GetOne",
+			Router:           "/:id",
 			AllowHTTPMethods: []string{"get"},
-			Params: nil})
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 
 	beego.GlobalControllerRouter["github.com/udistrital/ruler_api/controllers:PredicadoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/ruler_api/controllers:PredicadoController"],
 		beego.ControllerComments{
-			Method: "Put",
-			Router: `/:id`,
+			Method:           "Put",
+			Router:           "/:id",
 			AllowHTTPMethods: []string{"put"},
-			Params: nil})
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 
 	beego.GlobalControllerRouter["github.com/udistrital/ruler_api/controllers:PredicadoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/ruler_api/controllers:PredicadoController"],
 		beego.ControllerComments{
-			Method: "Delete",
-			Router: `/:id`,
+			Method:           "Delete",
+			Router:           "/:id",
 			AllowHTTPMethods: []string{"delete"},
-			Params: nil})
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 
 	beego.GlobalControllerRouter["github.com/udistrital/ruler_api/controllers:TipoPredicadoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/ruler_api/controllers:TipoPredicadoController"],
 		beego.ControllerComments{
-			Method: "Post",
-			Router: `/`,
+			Method:           "Post",
+			Router:           "/",
 			AllowHTTPMethods: []string{"post"},
-			Params: nil})
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 
 	beego.GlobalControllerRouter["github.com/udistrital/ruler_api/controllers:TipoPredicadoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/ruler_api/controllers:TipoPredicadoController"],
 		beego.ControllerComments{
-			Method: "GetOne",
-			Router: `/:id`,
+			Method:           "GetAll",
+			Router:           "/",
 			AllowHTTPMethods: []string{"get"},
-			Params: nil})
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 
 	beego.GlobalControllerRouter["github.com/udistrital/ruler_api/controllers:TipoPredicadoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/ruler_api/controllers:TipoPredicadoController"],
 		beego.ControllerComments{
-			Method: "GetAll",
-			Router: `/`,
+			Method:           "GetOne",
+			Router:           "/:id",
 			AllowHTTPMethods: []string{"get"},
-			Params: nil})
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 
 	beego.GlobalControllerRouter["github.com/udistrital/ruler_api/controllers:TipoPredicadoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/ruler_api/controllers:TipoPredicadoController"],
 		beego.ControllerComments{
-			Method: "Put",
-			Router: `/:id`,
+			Method:           "Put",
+			Router:           "/:id",
 			AllowHTTPMethods: []string{"put"},
-			Params: nil})
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 
 	beego.GlobalControllerRouter["github.com/udistrital/ruler_api/controllers:TipoPredicadoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/ruler_api/controllers:TipoPredicadoController"],
 		beego.ControllerComments{
-			Method: "Delete",
-			Router: `/:id`,
+			Method:           "Delete",
+			Router:           "/:id",
 			AllowHTTPMethods: []string{"delete"},
-			Params: nil})
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 
 }
