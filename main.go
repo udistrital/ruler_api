21d797e13a1d4dac70f9244bbd1ad4a9941d77ee
newkrux <newkrux@gmail.com>
2024-05-14 15:59:05 -0500

    fix: Ajuste para corrección en despliegue

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import (	
+import (
+	"net/url"
+	
 	_ "github.com/udistrital/ruler_api/routers"
 
 	"github.com/astaxie/beego"
@@ -11,7 +13,7 @@ import (
 
 func init() {
 	orm.RegisterDataBase("default", "postgres", "postgres://"+beego.AppConfig.String("db_user")+":"+
-																							beego.AppConfig.String("db_pass")+"@"+
+																							url.QueryEscape(beego.AppConfig.String("db_pass"))+"@"+
 																							beego.AppConfig.String("db_url")+"/"+
 																							beego.AppConfig.String("db_name")+"?sslmode=disable&search_path="+
 																							beego.AppConfig.String("db_schemas")+"")
